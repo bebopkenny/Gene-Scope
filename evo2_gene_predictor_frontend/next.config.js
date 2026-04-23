5974b2e95e650c0312af9596a922c717e71388f1
Kenny Garcia <kennygarcia15@yahoo.com>
2026-04-23 13:20:54 -0700

    setting up wiring to deploy on github pages

--- evo2_gene_predictor_frontend/next.config.js
+++ evo2_gene_predictor_frontend/next.config.js
@@ -4,9 +4,19 @@
  */
 import "./src/env.js";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "Gene-Scope";
+
 /** @type {import("next").NextConfig} */
 const config = {
     reactStrictMode: false,
+    output: "export",
+    trailingSlash: true,
+    images: { unoptimized: true },
+    eslint: { ignoreDuringBuilds: true },
+    ...(isProd
+        ? { basePath: `/${repoName}`, assetPrefix: `/${repoName}/` }
+        : {}),
 };
 
 export default config;
